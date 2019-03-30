29d9980739c4ac416af858155b9c1fe043bff442
Andres Martinez Martinez <andresmtzmtz@MacBook-Air-de-Andres.local>
2019-03-29 21:33:19 -0600

    quitealgo

--- practicas/2/MartinezAndres/Holamundo.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h> //Se incluye la biblioteca estandar de entrada y salida
-int main(){ //Se agrega la funcion main a partir de la cual se ejecutara el codigo
-	printf("HOLA MUNDO\n"); //Se usa la funcion printf para imprimir en pantalla la cadena HOLA MUNDO
-} //Hasta aca llega la funcion main 
